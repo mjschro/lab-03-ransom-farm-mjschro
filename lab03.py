173c6b18eabd169e562e9dd80517fbd98b9f4354
mjschro <mjschro@clemson.edu>
2026-01-29 14:46:38 -0500

    lab complete one

--- lab03.py
+++ lab03.py
@@ -1,18 +1,24 @@
-# TODO: Something should go up here...
+"""
+NAME: Michael Schroeder
+DATE: 1/29/2026
+CLASS: 1051
+SECTION: N/A
+
+"""
 
 # TODO: Fix frog string
 "    __   ___.--'_`."
 "   ( _`.'. -   'o` )"
-"   _\.'_'      _.-'"
-"  ( \`. )    //\`"
-"   \_`-'`---'\__,"
-"    \`        `-\"
+"   _\\.'_'      _.-'"
+"  ( \\`. )    //\\`"
+"   \\_`-'`---'\\__,"
+"    \\`        `-\\"
 
 # TODO: Fix sheep string
 "         __  _"
 "     .-:'  `; `-._"
 "    (_,           )"
-"  ,'o"(            )>"
+"  ,'o\"(            )>"
 " (__,-'            )"
 "    (             )"
 "     `-'._.--._.-'"
@@ -20,11 +26,49 @@
 
 
 def frog(): # TODO: implement frog()
-    pass
+    """
+    Returns ASCII art of a frog
+
+    Parameters:
+        None
+    
+    Returns:
+        String representing ASCII art of a frog
+    """
+
+    frog_string = (
+    "    __   ___.--'_`.\n"
+    "   ( _`.'. -   'o` )\n"
+    "   _\\.'_'      _.-'\n"
+    "  ( \\`. )    //\\`\n"
+    "   \\_`-'`---'\\__,\n"
+    "    \\`        `-\\\n"
+    )
+    return frog_string
 
 def sheep():  # TODO: implement sheep()    
-    pass
+    """
+    Returns ASCII art of a sheep
+
+    Parameters:
+        None
     
+    Returns:
+        String representing ASCII art of a sheep
+    """
+
+    sheep_string = (
+    "         __  _\n"
+    "     .-:'  `; `-._\n"
+    "    (_,           )\n"
+    "  ,'o\"(            )>\n"
+    " (__,-'            )\n"
+    "    (             )\n"
+    "     `-'._.--._.-'\n"
+    "        |||  |||\n"
+    )
+    return sheep_string
+
 def cow(): # This function is already implemented!
     """
     Returns ASCII art of a cow
@@ -61,20 +105,33 @@ def animal_n_times(func, n):
     """
     return func * n
 
+def farm(num_cows, num_frogs, num_sheep):
+    cow_strings = animal_n_times(cow(), num_cows)
+    frog_strings = animal_n_times(frog(), num_frogs)
+    sheep_strings = animal_n_times(sheep(), num_sheep)
+    total = cow_strings + frog_strings + sheep_strings
+    return total
+
 if __name__ == "__main__":
-   print("-------- Welcome to the Ransom Farm! --------\n")
-   print("Below is our comprehensive assortment of animals:\n")
-   print(cow())
+    print("-------- Welcome to the Ransom Farm! --------\n")
+    print("Below is our comprehensive assortment of animals:\n")
+    print(cow())
    
-   #TODO: print frog and sheep
+    #TODO: print frog and sheep
+    print(frog())
+    print(sheep())
+
+    #TODO: Take input from the user
+    print("Ol' farmer Ransom has a lot of animals!") 
+    print("How many cows would you like to see? ")
+    num_cows = int(input())
+    print("How many frogs would you like to see? ")
+    num_frogs = int(input())
+    print("How many sheep would you like to see? ")
+    num_sheep = int(input())
    
-   #TODO: Take input from the user
-   # print("Ol' farmer Ransom has a lot of animals!") 
-   # print("How many cows would you like to see? ")
-   # print("How many frogs would you like to see? ")
-   # print("How many sheep would you like to see? ")
-   
-   #TODO: place farm here
-   
-   # print("Thanks for stopping by the Ransom Farm! Come back soon!")
+    #TODO: place farm here
+
+    print(farm(num_cows, num_frogs, num_sheep))
+    print("Thanks for stopping by the Ransom Farm! Come back soon!")
     
